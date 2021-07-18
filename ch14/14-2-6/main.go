5429cec3beb3721051ff9704553bd994b7255d3c
masamichhhhi <masamichhhi@gmail.com>
2021-07-18 23:52:32 +0900

    add 14-6-2

---
+++ ch14/14-2-6/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	tasks := []string{
+		"cmake ..",
+		"cmake . --build Release",
+		"cpack",
+	}
+
+	var wg sync.WaitGroup
+	for _, task := range tasks {
+		go func(task string) {
+			fmt.Println(task)
+			wg.Done()
+		}(task)
+	}
+
+	wg.Wait()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/masamichhhhi/go-systemprograming
+
+go 1.16
+
+require golang.org/x/tools/gopls v0.7.0 // indirect
